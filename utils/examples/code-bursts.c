241146fcccf42f47e857973657478cd6891bd46e
Pisces000221 <1786762946@qq.com>
2015-07-11 22:38:28 +0800

    Use constants in the video generator example

--- utils/examples/code-bursts.c
+++ utils/examples/code-bursts.c
@@ -3,6 +3,11 @@
 // $ gcc code-bursts.c frame-pusher.o -o code-bursts `pkg-config --cflags --libs libavformat libswscale libswresample`
 
 #include "../frame-pusher.h"
+#define VID_WIDTH  640
+#define VID_HEIGHT 400
+#define VID_DUR    10
+#define VID_FRATE  30
+#define AUD_SRATE  44100
 
 int main(int argc, char *argv[])
 {
@@ -16,35 +21,34 @@ int main(int argc, char *argv[])
 
     int ret;
     frame_pusher *pusher;
-    // 44100 Hz / 30 fps, size = 640x400, bitrate = 800000
-    // The duration will be 10s
-    if ((ret = frame_pusher_open(&pusher, argv[1], 44100, 30, 640, 400, 800000)) < 0) {
+    if ((ret = frame_pusher_open(&pusher, argv[1], AUD_SRATE, VID_FRATE, VID_WIDTH, VID_HEIGHT, 800000)) < 0) {
         av_log(NULL, AV_LOG_ERROR, "Cannot open output\n");
         return ret;
     }
 
     uint8_t *picture[4] = { 0 };
-    int linesize[4] = { 0 }, bufsize = 640 * 3 * 400 * sizeof(uint8_t);
+    // The buffer size needs to be multiplied by 3 because the format is RGB24
+    int linesize[4] = { VID_WIDTH * 3 * sizeof(uint8_t) };
+    int bufsize = VID_HEIGHT * linesize[0];
     picture[0] = (uint8_t *)av_malloc(bufsize);
     picture[1] = (uint8_t *)av_malloc(bufsize);
-    linesize[0] = 640 * 3 * sizeof(uint8_t);
 
     // Generate frames
     int i, j;
     uint8_t grey;
     float loudness;
-    for (i = 0; i < 300; ++i) {
+    for (i = 0; i < VID_FRATE * VID_DUR; ++i) {
         // Video
         for (j = linesize[0] * i; j < linesize[0] * (i + 1); ++j)
             picture[0][j] = random() % 256;
         memset(picture[0] + linesize[0] * (i + 1), i < 150 ? 255 : 0, linesize[0]);
-        grey = (int)((double)i / 299.0 * 255.0);
+        grey = (int)((double)i / (double)(VID_FRATE * VID_DUR - 1) * 255.0);
         memset(picture[0] + linesize[0] * (i + 2), grey, bufsize);
         frame_pusher_write_video(pusher, picture, linesize, 1);
         // Audio
         loudness = ((1 + sin((float)i / 75.0 * M_PI)) / 2);
         loudness = loudness * loudness * 2 + 0.2;
-        for (j = 0; j < 441000 / 300; ++j)
+        for (j = 0; j < AUD_SRATE / VID_FRATE; ++j)
             frame_pusher_write_audio(pusher,
                 (int16_t)(loudness * (float)(random() % 200 - 100)),
                 (int16_t)(loudness * (float)(random() % 200 - 100)));
