48eb9e4abf6efeeef83d6764929c9ffaea15754b
Pisces000221 <1786762946@qq.com>
2015-09-21 23:16:34 +0800

    Finish document for the newly-added method

--- utils/frame-puller.h
+++ utils/frame-puller.h
@@ -91,8 +91,9 @@ int frame_puller_seek(frame_puller *fp, float time, unsigned char precise);
 int frame_puller_seek_timestamp(frame_puller *fp, int64_t timestamp, unsigned char precise);
 
 /**
- * Get the
+ * Get the display time (in seconds) of the last pulled frame.
  *
+ * @param[in]  fp  The pointer to the struct to be used.
  */
 float frame_puller_last_time(frame_puller *fp);
 
